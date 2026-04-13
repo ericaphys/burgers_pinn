799714771e04e808bc508249816c39ef1f28c9dd
erica <novelerica@gmail.com>
2026-04-13 19:20:01 +0200

    initial commit

---
+++ burgers.py
@@ -0,0 +1,7 @@
+import torch 
+from torch import nn
+import numpy as np
+from torch.utils.data import DataLoader
+from torch.utils.data import TensorDataset
+from torchvision import datasets, transforms
+import matplotlib.pyplot as plt
